678f990c21545576ca9ab93fdac8f6de0bd1f4d7
Vinicius Resende <alive2k@programmer.net>
2021-04-10 14:04:17 -0300

    force redeply

--- src/services/API.ts
+++ src/services/API.ts
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const baseURL = process.env.BASE_API_URL || "http://localhost:3000/api/v1"
+const baseURL = process.env.BASE_API_URL || "https://encore-bleszerd.vercel.app/api/v1/"
 
 const API = axios.create({
     baseURL
