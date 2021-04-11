a287212857d3241ec40e89851957e5adcdba2ab1
Vinicius Resende <alive2k@programmer.net>
2021-04-11 09:07:55 -0300

    push again

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 module.exports = {
     env: {
-        BASE_API_URL: process.env.BASE_API_URL || "http://localhost:3000/api/v1/",
-        MONGODB_URI: process.env.MONGODB_URI ||"mongodb+srv://bleszerd:senhasegura2@cluster0.jq6gi.mongodb.net/encore?retryWrites=true&w=majority",
+        BASE_API_URL: "encore-theta.vercel.app/api/v1/",
+        MONGODB_URI: "mongodb+srv://bleszerd:senhasegura2@cluster0.jq6gi.mongodb.net/encore?retryWrites=true&w=majority",
     },
-  };
+};
+
