47a28a1ac1b6da679f3a8c13981b6bece66a27e8
Vinícius Resende <77847606+bleszerd@users.noreply.github.com>
2021-04-23 16:00:13 -0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
 module.exports = {
     env: {
         BASE_API_URL: process.env.BASE_API_URL || "http://localhost:3000/api/v1/",
-        MONGODB_URI: process.env.MONGODB_URI || "MONGO_LOCAL_URI",
-        JWT_KEY: process.env.JWT_KEY || "superSeNhASegura2021Pog"
+        MONGODB_URI: process.env.MONGODB_URI,
+        JWT_KEY: process.env.JWT_KEY || "JWT_KEY_SECRET@@@@"
     },
 };
 
