a4136b36de02591ced1efd47e8f37ba3388696bf
Vinícius Resende <77847606+bleszerd@users.noreply.github.com>
2021-04-23 15:59:53 -0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 module.exports = {
     env: {
         BASE_API_URL: process.env.BASE_API_URL || "http://localhost:3000/api/v1/",
-        MONGODB_URI: process.env.MONGODB_URI || "mongodb+srv://bleszerd:senhasegura2@cluster0.jq6gi.mongodb.net/encore?retryWrites=true&w=majority",
+        MONGODB_URI: process.env.MONGODB_URI || "MONGO_LOCAL_URI",
         JWT_KEY: process.env.JWT_KEY || "superSeNhASegura2021Pog"
     },
 };
